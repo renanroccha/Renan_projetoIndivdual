2aca5b6d95c7b1ffff35cf7aeca1586ef6429b07
renanroccha <renan.rocha@sptech.school>
2023-11-21 15:53:48 -0300

    adicionar validação

--- webdataviz/site/public/js/sessao.js
+++ webdataviz/site/public/js/sessao.js
@@ -17,20 +17,20 @@ function limparSessao() {
     window.location = "../login.html";
 }
 
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
+// // carregamento (loading)
+// function aguardar() {
+//     // var divAguardar = document.getElementById("div_aguardar");
+//     // divAguardar.style.display = "flex";
+// }
 
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
+// function finalizarAguardar(texto) {
+//     // var divAguardar = document.getElementById("div_aguardar");
+//     // divAguardar.style.display = "none";
 
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
+//     var divErrosLogin = document.getElementById("div_erros_login");
+//     if (texto) {
+//         divErrosLogin.style.display = "flex";
+//         divErrosLogin.innerHTML = texto;
+//     }
+// }
 
--- webdataviz/site/src/controllers/usuarioController.js
+++ webdataviz/site/src/controllers/usuarioController.js
@@ -55,22 +55,22 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var regiao = req.body.regiaoServer;
     var senha = req.body.senhaServer;
-    var empresaId = req.body.empresaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
+    } else if (regiao == undefined) {
+        res.status(400).send("Sua regiao está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (empresaId == undefined) {
-        res.status(400).send("Sua empresa está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, empresaId)
+        usuarioModel.cadastrar(nome, email, regiao, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
